40f25dc7f7f29ac89659ffdbc5388ba893cb89ab
Kalyan Mantripragada <nani.kalyan90@gmail.com>
2025-05-17 23:22:20 +0530

    Integrate react markdown and rehype highlight

--- frontend/src/components/ResponseBox.tsx
+++ frontend/src/components/ResponseBox.tsx
@@ -1,4 +1,6 @@
+import rehypeHighlight from "rehype-highlight";
 import type { Message } from "../types";
+import ReactMarkdown from 'react-markdown'
 
 interface ResponseBoxProps {
   messages: Message[];
@@ -10,7 +12,8 @@ export default function ResponseBox({ messages }: ResponseBoxProps) {
     {
       messages && messages.map((message, index) => (
         <div key={index} style={{ margin: '10px', padding: '10px', color: message.role === 'User' ? '#e0f7fa' : '#ffe0b2' }}>
-          <strong>{message.role}:</strong> {message.content}
+          <strong>{message.role}:</strong> 
+          <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{message.content}</ReactMarkdown>
         </div>
       ))
     }
